2aab3261bf347d4248a5a331527905d09087221a
Gursharan Singh <gursharan@vsrkcapital.com>
2022-10-19 15:45:54 +0530

    remove dotenv

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import express from "express";
 import expressLayouts from 'express-ejs-layouts';
 import path from 'path';
 import indexRoute from '../routes/indexRoute.js'
 import mongoose from 'mongoose';
 
+const PORT = process.env.port??'4500'
+const DB_URL = process.env.DATABASE_URL??'mongodb://localhost:27017'
 const app = express()
 
 app.use(express.static(path.join(process.cwd(),'/public')))
@@ -17,7 +16,7 @@ app.set('views', path.join(process.cwd(),'resources/views'))
 app.use(expressLayouts)
 app.set('layout', path.join(process.cwd(),'resources/layouts/guestLayout'))
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(DB_URL)
 const db = mongoose.connection
 db.on('error', error => console.error(error) )
 db.once('open', ()=>console.log('connect to DB'))
